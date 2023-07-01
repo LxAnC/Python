974ee8193f25210967038d1ac2394831d13f2582
LuXuAn <5001363@qq.com>
2023-07-01 21:33:15 +0800

    运算符和输入

---
+++ 输入以及运算符.py
@@ -0,0 +1,51 @@
+# 时间：2023/7/1 20:54
+# 作者：LxAnC
+# 输入函数input
+# present=input('大圣想要什么礼物呢')
+# print(present)
+# # 课后作业
+# a=int(input('输入第一个数'))
+# b=int(input('输入第二个数'))
+# print('他们的和是',a+b)
+
+# 运算符
+# 标准运算符
+# print(1+1)
+# print(1-1)
+# print(1*5)
+# print(2/3)
+# print(2//3)
+# print(2%3)
+# print(2**8)
+
+# 特别注意：一正一负的情况
+# print(9//-2)
+# print(-9//2)
+# print(9%-4)
+
+# 赋值运算符 从右到左
+# a=3+4
+# print(a)
+# 链式赋值
+# a=b=c=20
+# print(a,type(a),b,type(b),c,type(c))
+# print(id(a),id(b),id(c))
+
+# 参数赋值
+# a=2
+# a*=2
+# print(a)
+# a/=2
+# print(a)
+# print(type(a))
+# a*=19
+# a//=3
+# 除的时候会变为float类型
+
+# 支持解包赋值//交换两个变量的值
+# a,b,c=20,30,40
+# print(a,b,c)
+# a,b=b,a
+# print(a,b)
+
+# 比较运算符
