5de85ff921775fd124f4f1c9a3d175b10207b629
LuXuAn <5001363@qq.com>
2023-07-01 19:29:28 +0800

    转义字符的开始

--- 数据类型转换.py
+++ 数据类型转换.py
@@ -4,4 +4,14 @@
 name="张三"
 age=20
 print(type(name),type(age))# 说明类型不相同
-print('我叫'+name+'今年'+age)
+# print('我叫'+name+'今年'+age+‘岁’)# 会报错
+# 使用str转换
+print('我叫'+name+'今年'+str(age)+'岁')# 不会报错
+# str int float这三种类型的类型转换
+print('---------------------------str()将其他类型转成str类型-----')
+a=10
+b=198.9
+c=False
+print(type(a),type(b),type(c))
+print(str(a),str(b),str(c))
+
