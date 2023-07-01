b7740958ed19b16aeab3fab3626f4d219c8e6f74
LuXuAn <5001363@qq.com>
2023-07-01 20:53:33 +0800

    注释(完)

---
+++ 注释.py
@@ -0,0 +1,12 @@
+# coding:utf-8 # 中文编码声明注释
+# py文件的默认编码是utf-8，如果要重新设置请添加coding：GBK则会显示ansi，ansi包含gbk
+# 时间：2023/7/1 20:43
+# 作者：LxAnC
+# 输出功能(单行注释)
+print('hello')
+# 多行注释
+'''
+嘿嘿
+我是多行注释
+'''
+# python的文件默认时utf-8
