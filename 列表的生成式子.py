604f4185838553314f3adb7f00259c76df8b2748
LuXuAn <5001363@qq.com>
2023-07-02 19:49:13 +0800

    列表生成式

---
+++ 列表的生成式子.py
@@ -0,0 +1,10 @@
+# 时间：2023/7/2 19:44
+# 作者：LxAnC
+
+# 列表生成式---生成列表的公式
+lst=[i for i in range(1,10)]
+print(lst)
+# 使用列表生成式必须要求有规律
+print('列表中的值为2，4，6，8……')
+lst=[a*2 for a in range(1,6)]
+print(lst)
