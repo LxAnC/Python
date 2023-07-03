d1e38241c74d249ece82e3a6f9b2bf8adbf50a5b
LuXuAn <5001363@qq.com>
2023-07-03 16:05:44 +0800

    学习了集合生成式

--- 6.集合生成式.py
+++ 6.集合生成式.py
@@ -2,9 +2,9 @@
 # 作者：LxAnC
 
 # 列表生成式
-lst=[ i*i for i in range(6)]
+lst=[ i*i for i in range(10)]
 print(lst)
 
 # 集合生成式
-s={ i*i for i in range(6)}
+s={ i*i for i in range(10)} # 顺序无序但内容一样
 print(s)
