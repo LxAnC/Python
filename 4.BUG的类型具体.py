631371de96aa51cbbfc5d3c9aa0d06cc712f8bec
LuXuAn <5001363@qq.com>
2023-07-05 22:26:47 +0800

    知道IndexError，SyntaxError

---
+++ 4.BUG的类型具体.py
@@ -0,0 +1,14 @@
+# 时间：2023/7/5 22:23
+# 作者：LxAnC
+# 零值异常 ZeroDivisioinError
+print(10/0)
+
+lst=[11,22,33,44]
+print(lst[4])
+# 报错 索引超出了IndexError
+dic={'name':'张三','age':20}
+print(dic['gender'])# KeyError 是字典中没有对应键所以报错
+
+# 没声明就是 NameError
+# int i=1是语法错误SyntaxError
+# a=int('hello') # ValueError传值异常
