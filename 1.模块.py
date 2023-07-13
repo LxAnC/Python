b61ef8281541f15d4f3ca7bcef5d86b7d438c113
LuXuAn <5001363@qq.com>
2023-07-13 19:06:30 +0800

    学习模块

---
+++ 1.模块.py
@@ -0,0 +1,23 @@
+# 时间：2023/7/13 19:01
+# 作者：LxAnC
+
+# 什么叫模块
+# modules  一个.py的文件就是一个模块，里面有很多很多的函数
+def fun():
+    pass
+def fun2():
+    pass
+class Student:
+    native_place='吉林'
+    @classmethod
+    def cm(cls):
+        pass
+    @staticmethod
+    def sm():
+        pass
+# 一个模块可以包含n多个类，n多个语句，以及函数
+# 一个项目里可能有n多个模块#
+# 最终组成了一个文件 叫做python程序
+'''
+为什么要采用模块化编程，实现代码复用
+'''
