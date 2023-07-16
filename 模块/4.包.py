8e8bc41536597e843e0fd263c7147055cd8ffaac
LuXuAn <5001363@qq.com>
2023-07-16 14:28:21 +0800

    包的导入

---
+++ 模块/4.包.py
@@ -0,0 +1,9 @@
+# 时间：2023/7/16 14:23
+# 作者：LxAnC
+# 在此文件下导入包
+import PythonPackage.moduleA
+print(PythonPackage.moduleA.a)
+# 太长了怎么办 后面设 as 别名
+# import PythonPackage.moduleA as a
+
+# 导入的本质
---
+++ 模块/5.包导入的本质.py
@@ -0,0 +1,10 @@
+# 时间：2023/7/16 14:25
+# 作者：LxAnC
+
+# 本质
+import PythonPackage
+import add
+# 使用import时导入，只能跟包名和模块名
+# from 包名 import moduleA
+# from PythonPackage.moduleA import a
+# 这种方式就可以导入包，模块，函数，变量
